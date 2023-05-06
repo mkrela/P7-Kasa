8affccacbcd9df9b79339fa1bdca2214e8f867cf
m.krela <m.krela@icloud.com>
2023-05-06 17:12:56 +0200

    add NavBar

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Banner/>
+    </div>
+  );
+}
+
+export default App;
