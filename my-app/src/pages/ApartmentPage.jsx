98b7c678dd4e180a41153007330a249cba023d06
m.krela <m.krela@icloud.com>
2023-05-10 17:15:18 +0200

    fix

--- my-app/src/pages/ApartmentPage.jsx
+++ my-app/src/pages/ApartmentPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import DescriptionPanel from "../components/DescriptionPanel";
 import "./ApartmentPage.scss";
 import { ImageBanner } from "../components/ImageBanner";
