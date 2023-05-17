4bdf311411c10c0f964c1e3be8d3991f88529c6a
m.krela <m.krela@icloud.com>
2023-05-18 01:56:41 +0200

    Add Hide Buttons if 1 pic

--- my-app/src/components/ImageBanner.jsx
+++ my-app/src/components/ImageBanner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ImageBanner.scss";
 
 export function ImageBanner(props) {
@@ -30,25 +30,49 @@ export function ImageBanner(props) {
 
   const getCarouselOrDefaultImage = () => {
     if (!arePicturesAvailable()) {
-      return <img src="../../public/aboutBackground.jpg" className="show" alt="" />;
+      return (
+        <img src="../../public/aboutBackground.jpg" className="show" alt="" />
+      );
     }
     return pictures.map((pic, i) => (
       <img key={pic} src={pic} alt="" className={getClassName(i)}></img>
     ));
   };
 
+  const hideButtonIfOnePicture = () => {
+    if (pictures.length === 1) {
+      const buttons = document.getElementsByClassName("bouton__carrousel");
+      const counter = document.getElementsByClassName("slide-counter");
+      console.log(counter);
+      counter[0].classList.add("hide__bouton");
+      for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.add("hide__bouton");
+      }
+    }
+  };
+
+  useEffect(() => {
+    hideButtonIfOnePicture();
+  }, []); // Empty dependency array ensures the effect runs only once after initial render
+
   return (
     <div className="image__banner">
       <div className="image__container">{getCarouselOrDefaultImage()}</div>
       {arePicturesAvailable() && (
         <>
-          <button className="btn btn-previous" onClick={moveToPrevious}>
+          <button
+            className="btn btn-previous bouton__carrousel"
+            onClick={moveToPrevious}
+          >
             <i className="fas fa-chevron-left"></i>
           </button>
           <span className="slide-counter">
             {currentPicture + 1} / {pictures.length}
           </span>
-          <button className="btn btn-next" onClick={moveToNext}>
+          <button
+            className="btn btn-next bouton__carrousel"
+            onClick={moveToNext}
+          >
             <i className="fas fa-chevron-right"></i>
           </button>
         </>
