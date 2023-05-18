84c529fb422a1897dee3ebb7c8e41b2e6f32c1f3
m.krela <m.krela@icloud.com>
2023-05-18 02:29:17 +0200

    adjusting the collapse system

--- my-app/src/components/DescriptionPanel.jsx
+++ my-app/src/components/DescriptionPanel.jsx
@@ -12,14 +12,14 @@ export function DescriptionPanel(props) {
   const chevronClass =
     (isContentVisible ? "fa-chevron-up" : "fa-chevron-down") + " fas";
   return (
-    <div>
-      <div className="description__panel">
+    <div style={{  zIndex: '0' }}>
+      <div className="description__panel" >
         <p className="description__header" onClick={showContent}>
           <span>{props.title}</span>
           <i className={chevronClass}></i>
         </p>
       </div>
-      <p className={contentClass}>{props.content}</p>
+      <p className={contentClass} style={{cursor: 'pointer' ,  zIndex: '1' }} onClick={showContent} >{props.content}</p>
     </div>
   );
 }
